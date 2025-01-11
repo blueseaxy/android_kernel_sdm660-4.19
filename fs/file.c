172dca93ea78f7f0d749aca85d7b703b4268607e
Cherry <99527152+blueseaxy@users.noreply.github.com>
2025-01-11 07:27:42 +0000

    Revert "BACKPORT: file: Rename __close_fd_get_file close_fd_get_file"
    This reverts commit 5faa0e4853751c4aff707dcebbd9a61e9b5d5eb4.

--- fs/file.c
+++ fs/file.c
@@ -641,37 +641,11 @@ out_unlock:
 EXPORT_SYMBOL(__close_fd); /* for ksys_close() */
 
 /*
- * See close_fd_get_file() below, this variant assumes current->files->file_lock
- * is held.
- */
-int __close_fd_get_file(unsigned int fd, struct file **res)
-{
-	struct files_struct *files = current->files;
-	struct file *file;
-	struct fdtable *fdt;
-
-	fdt = files_fdtable(files);
-	if (fd >= fdt->max_fds)
-		goto out_err;
-	file = fdt->fd[fd];
-	if (!file)
-		goto out_err;
-	rcu_assign_pointer(fdt->fd[fd], NULL);
-	__put_unused_fd(files, fd);
-	get_file(file);
-	*res = file;
-	return 0;
-out_err:
-	*res = NULL;
-	return -ENOENT;
-}
-
-/*
- * variant of close_fd that gets a ref on the file for later fput.
+ * variant of __close_fd that gets a ref on the file for later fput.
  * The caller must ensure that filp_close() called on the file, and then
  * an fput().
  */
-int close_fd_get_file(unsigned int fd, struct file **res)
+int __close_fd_get_file(unsigned int fd, struct file **res)
 {
 	struct files_struct *files = current->files;
 	int ret;
